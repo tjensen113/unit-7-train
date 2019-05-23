e096cad0d5eceae2f83986e50f462479f9ae72fc
Thomas Jensen <thomasjensen@Thomass-MacBook-Pro.local>
2019-05-22 22:02:36 -0500

    psuedo code set for js page

---
+++ assets/app.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+
+ // Initialize Firebase
+var config={
+
+}
+
+var database = firebase.database();
+
+//Initializing the variables .....................
+
+
+
+
+//Capturing the add train button click.................
+
+
+
+
+ //Declarethe variables that will hold the user input values..............................
+
+
+  //Creating a local temporary object for holding train info
+
+
+  //Upload the train data to the database.....
+
+
+
+   //Clearing all the values from the input area when the submit button is clicked.
+
+
+
+
+
+
+
+   //Creating a firebase event for adding train to the database and a row to the html..........
+
+
+
+
+
+
+
+     //Store to a variable.......
+
+
+
+
+      //Use moment.js to convert the first train arrival time to .......
+
+
+
+
+
+
+
+       //Use moment.js to show current time......
+
+
+
+
+       //Declaring a variable that will hold the dynamically created rows and table data elements with its values....
+
+
+
+        //Appending the row to the table body..
+
+
+
+
+});
